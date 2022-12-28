ceab0c951d707035a52b2d7d090256d6c99c2897
roshanbarkuntwar <98811584+roshanbarkuntwar@users.noreply.github.com>
2022-12-28 11:56:09 +0530

    Add files via upload

---
+++ Ansible-installation.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+sudo apt-add-repository ppa:ansible/ansible
+
+sudo apt update -y
+
+sudo apt install ansible -y
+
+sudo apt-get install nfs-utils common -y
+
+sudo apt install git -y
+
+sudo apt upgrade -y
